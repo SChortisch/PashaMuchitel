4263ffc5003ab0282e61fdd6a6e1583ee800c5d6
SChortisch <coolyesman193@gmail.com>
2024-01-16 16:59:21 +0200

    pasha commit

---
+++ 2.py
@@ -0,0 +1,21 @@
+#use only yes/no
+input_funct1 = input('Is Pasha pidoras?')
+ifyes = 'correct, good boy!!!'
+ifno = 'Nah, bad boy!!!'
+if input_funct1 == 'yes':
+    print(ifyes)
+while input_funct1 == 'no':
+    print(ifno)
+    input_funct2 = input('are you sure?')
+    if input_funct2 == 'no':
+        print(ifyes)
+    while input_funct2 == 'yes':
+        print('Mmmmmmmmmmmmmm')
+        input_funct3 = input('are you fucking realy sure, you, stupid asshole?')
+        if input_funct3 == 'no':
+            print(ifyes)
+        while input_funct3 == 'yes':
+            print('fuck you, you walking bitchinness')
+        
+
+   
